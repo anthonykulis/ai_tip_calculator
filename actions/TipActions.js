e1577c4ed8db04f5d9863e46fd86d9c1766eba16
Anthony Kulis <anthony.kulis@gmail.com>
2016-06-30 16:55:58 -0500

    fixes spellign error

--- actions/TipActions.js
+++ actions/TipActions.js
@@ -6,7 +6,7 @@ let TipActions = {
 
   // When the calculate button is pressed, we will call this action.
   // it will then disptach the event CALCULATE_TIP with data
-  calcuateTip: function(bill_amount, tip_percent){
+  calculateTip: function(bill_amount, tip_percent){
     TipDispatcher.disptach({
       type: TipConstants.CALCULATE_TIP,
       bill_amount: bill_amount,
